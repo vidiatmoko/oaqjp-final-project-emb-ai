a2082ba77bb0030c83cae96ebb3675876307a44b
vidiatmoko <vidiatmoko1@gmail.com>
2026-08-08 09:28:03 -0400

    Complete Task 3: Format emotion detector output

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,4 +7,29 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
     
     response = requests.post(url, json=myobj, headers=headers)
-    return response.text
+    
+    # Mengubah string JSON dari respon API menjadi kamus (dictionary) Python
+    formatted_response = json.loads(response.text)
+    
+    # Mengambil objek emosi dari struktur respons Watson API
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    
+    # Mengambil nilai skor masing-masing emosi
+    anger_score = emotions['anger']
+    disgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness']
+    
+    # Logika untuk mencari emosi dominan (skor tertinggi)
+    dominant_emotion = max(emotions, key=emotions.get)
+    
+    # Mengembalikan format kamus yang diminta
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
