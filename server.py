67b02ccd38a162416ff6f8d5103da9e60adad26c
vidiatmoko <vidiatmoko1@gmail.com>
2026-08-08 11:00:16 -0400

    Add Task 6 server and deployment screenshot

---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    # Mengambil teks input dari argumen permintaan HTTP
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    # Memanggil fungsi emotion_detector
+    response = emotion_detector(text_to_analyze)
+
+    # Memeriksa jika emosi dominan None atau kosong
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+
+    # Memformat respons sesuai dengan spesifikasi tugas
+    return (
+        f"For the given statement, the system response is 'anger': {response['anger']}, "
+        f"'disgust': {response['disgust']}, 'fear': {response['fear']}, "
+        f"'joy': {response['joy']} and 'sadness': {response['sadness']}. "
+        f"The dominant emotion is {response['dominant_emotion']}."
+    )
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
